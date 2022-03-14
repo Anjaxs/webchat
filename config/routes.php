a5390aec7b38b96787c259a8efffc3f7dc19911b
anjaxs <942403648@qq.com>
2022-03-14 14:26:26 +0800

    去掉view相关组件，引入前端项目webchat

--- config/routes.php
+++ config/routes.php
@@ -11,12 +11,6 @@ declare(strict_types=1);
  */
 use Hyperf\HttpServer\Router\Router;
 
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
-
 Router::addServer('ws', function () {
     Router::get('/', 'App\Controller\WebSocketController');
-});
-
-Router::get('/favicon.ico', function () {
-    return '';
 });
